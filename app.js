3b18b3eda1d4ba4c1e3a979223f98849a2b5b6f5
MaxAntony <maxpacami@gmail.com>
2023-05-29 04:18:48 -0500

    bot temporal

--- app.js
+++ app.js
@@ -8,8 +8,18 @@ const {
 const MetaProvider = require("@bot-whatsapp/provider/meta");
 const MongoAdapter = require("@bot-whatsapp/database/mongo");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const AllMessagesSchema = new Schema({
+  from: String,
+  to: String,
+  body: String,
+});
 
 const {
   MONGO_DB_URI,
@@ -19,75 +29,19 @@ const {
   META_VERIFY_TOKEN,
 } = process.env;
 
-const flowSecundario = addKeyword(["2", "siguiente"]).addAnswer([
-  "📄 Aquí tenemos el flujo secundario",
-]);
-
-const flowDocs = addKeyword([
-  "doc",
-  "documentacion",
-  "documentación",
-]).addAnswer(
-  [
-    "📄 Aquí encontras las documentación recuerda que puedes mejorarla",
-    "https://bot-whatsapp.netlify.app/",
-    "\n*2* Para siguiente paso.",
-  ],
-  null,
-  null,
-  [flowSecundario]
-);
-
-const flowTuto = addKeyword(["tutorial", "tuto"]).addAnswer(
-  [
-    "🙌 Aquí encontras un ejemplo rapido",
-    "https://bot-whatsapp.netlify.app/docs/example/",
-    "\n*2* Para siguiente paso.",
-  ],
-  null,
-  null,
-  [flowSecundario]
-);
-
-const flowGracias = addKeyword(["gracias", "grac"]).addAnswer(
-  [
-    "🚀 Puedes aportar tu granito de arena a este proyecto",
-    "[*opencollective*] https://opencollective.com/bot-whatsapp",
-    "[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez",
-    "[*patreon*] https://www.patreon.com/leifermendez",
-    "\n*2* Para siguiente paso.",
-  ],
-  null,
-  null,
-  [flowSecundario]
-);
-
-const flowDiscord = addKeyword(["discord"]).addAnswer(
-  [
-    "🤪 Únete al discord",
-    "https://link.codigoencasa.com/DISCORD",
-    "\n*2* Para siguiente paso.",
-  ],
-  null,
-  null,
-  [flowSecundario]
-);
-
-const flowPrincipal = addKeyword(["hola", "ole", "alo"])
-  .addAnswer(
-    "🙌 ¡Hola! ¡Gracias por tu interés en nuestros servicios! Estoy aquí para ayudarte. ¿En qué puedo asistirte?"
-  )
-  .addAnswer(
-    [
-      "te comparto los siguientes links de interes sobre el proyecto",
-      "👉 ** para ver la documentación",
-      "👉 *gracias*  para ver la lista de videos",
-      "👉 *discord* unirte al discord",
-    ],
-    null,
-    null,
-    [flowDocs, flowGracias, flowTuto, flowDiscord]
-  );
+const flowPrincipal = addKeyword([
+  "hola",
+  "ole",
+  "alo",
+  "Hola, quisiera hablar sobre un proyecto",
+  "buenos",
+  "buenas",
+])
+  .addAnswer("🙌 ¡Hola! ¡Gracias por tu interés en nuestros servicios!")
+  .addAnswer([
+    "Este bot esta siendo actualizado por lo que no estara disponible unos dias 😞",
+    "pero no te preocupes, en breve se comunicara contigo un asesor 😉",
+  ]);
 
 const main = async () => {
   const adapterDB = new MongoAdapter({
@@ -102,6 +56,18 @@ const main = async () => {
     numberId: META_NUMBER_ID,
     verifyToken: META_VERIFY_TOKEN,
     version: "v16.0",
+  });
+
+  await mongoose.connect(`${MONGO_DB_URI}/messages`);
+  const AllMessages = mongoose.model("AllMessages", AllMessagesSchema);
+
+  adapterProvider.on("message", (ctx) => {
+    console.log("=========");
+    console.log(ctx);
+    const newMessage = new AllMessages(ctx);
+    newMessage.save();
+
+    console.log("=========");
   });
 
   createBot({
